6f5f03c478dfc0ebf562effd40e8833152f18bf5
codywelller <cweller4@gmu.edu>
2024-03-11 12:02:41 -0400

    second try

---
+++ js/report.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", (event) => {
+  document.querySelector("#run").addEventListener("click", function() {
+      validateform()
+    }
+  );
+});
+
+function validateform() {
+  let x = document.forms["reportform"]["producer"].value;
+  let y = document.forms["reportform"]["dj"].value;
+  let test = 0;
+  if (x == "") {
+    test = 1
+  }
+  if (y == "") {
+    test = test + 2
+  }
+  console.log(test)
+  switch (test){
+	  case 1:
+		alert("Producer must be filled out");
+		return false;
+	  case 2:
+		alert("DJ must be filled out");
+		return false;
+	  case 3:
+	  alert("Producer and DJ must be filled out");
+		return false;
+	  default:
+		break;
+  }
+}
