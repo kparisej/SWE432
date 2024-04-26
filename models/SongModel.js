befc599c0cbe8caf07eea750c90e6400b503c712
karenparisejas <kparisej@gmu.edu>
2024-04-25 23:14:25 -0400

    updated database connection to shared database.

--- models/SongModel.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const songsSchema = new mongoose.Schema({
-  songName: String,
-  artist: String,
-  genre: String,
-  duration: String,
-});
-
-module.exports = mongoose.model("SongModel", songsSchema, "songs");
